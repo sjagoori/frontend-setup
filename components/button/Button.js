2f0fd85ae12b12a71e4b67ce593d2c055f7ac557
sjagoori <sjagoori@users.noreply.github.com>
2020-12-21 17:04:32 +0100

    Created button component

---
+++ components/button/Button.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import styled, { ThemeProvider } from 'styled-components';
+import theme from '../../styles/theme'
+import Icon from '../../assets/svg/chevron-right'
+
+export default class Button extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <a href={this.props.url}>
+          <Container variant={this.props.variant}>
+            <Icon />
+            <span>{this.props.label}</span>
+          </Container>
+        </a>
+      </ThemeProvider>
+    )
+  }
+}
+
+const Container = styled.div`
+  color: ${props => {
+    if (props.variant == 'primary') return props.theme.colors.white;
+    if (props.variant == 'secondary' || props.variant == 'ghost') return props.theme.colors.black;
+  }};
+
+  background-color: ${props => {
+    if (props.variant == 'primary') return props.theme.colors.primary;
+    if (props.variant == 'secondary') return props.theme.colors.secondary;
+  }};
+
+  border: ${props => {
+    if (props.variant == 'ghost') return '2px solid ' + props.theme.colors.black;
+  }};
+
+  text-transform: capitalize;
+  font-size: .9em;
+  cursor: pointer;
+  display: block;
+  width: 150px;
+  height: 30px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    margin-left: -15px;
+    fill: ${props => {
+      if (props.variant == 'primary') return props.theme.colors.white;
+      if (props.variant == 'secondary' || props.variant == 'ghost') return props.theme.colors.black;
+    }};
+    opacity: ${props => props.variant == 'text' ? 1 : 0};
+    margin-right: 10px;
+    transition:  opacity .1s ease-in-out;
+  }
+
+  :hover{
+     svg {
+      opacity: 1;
+    }
+  }
+`
---
+++ components/button/README.md
@@ -0,0 +1,37 @@
+# Button component
+
+## Table of contents
+  - [What it does](#what-it-does)
+  - [How to use](#how-to-use)
+  - [Dependencies](#dependencies)
+  - [Developer](#developer)
+  - [Changelog](#changelog)
+
+<div style="text-align:center"><img src="screenshot.png" alt="alt text" width="300"/></div>
+
+## What it does 
+* Generate a button
+
+## How to use
+Import and implement component
+
+```js
+import Button from '../components/button/Button'
+
+<Button
+  label='label'     // required
+  url='#'           // required
+  variant='ghost'   // required 
+/>
+```
+
+## Dependencies
+* [Styled-components](https://styled-components.com/)
+
+## Developer
+[Shabier](https://www.github.com/sjagoori)
+
+## Changelog
+
+### 1.0
+* Initial creation
