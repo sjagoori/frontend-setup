c7de40a9db5e978b1fde303f5ad765e8a4db89d7
sjagoori <sjagoori@users.noreply.github.com>
2020-12-21 17:04:06 +0100

    Created inital theming scheme

---
+++ styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    primary: "#1D1D1D",
+    secondary: "#CCCCCC",
+    black: "#1D1D1D",
+    white: "white",
+    grey: "#CCCCCC",
+    red: "red",
+    green: "#27CB73",
+    yellow: '#FFCC00'
+  },
+  backgroundColor:'#FBF1F4'
+}
